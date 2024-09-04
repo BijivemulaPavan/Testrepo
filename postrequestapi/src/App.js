c5d9b57ff6033b6adf7b0962161476f85dc6b8b5
BijivemulaPavan <144310731+BijivemulaPavan@users.noreply.github.com>
2024-09-04 17:59:12 +0530

    Add files via upload

---
+++ postrequestapi/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import AddPost from "./AddItems";
+
+function App() {
+  return (
+    
+      <div>
+        <h1>Post</h1>
+        <AddPost/>
+        
+      </div>
+    
+    
+  );
+}
+
+export default App;
